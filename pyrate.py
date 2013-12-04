423aceece4574c80d785a81683aacd0f3fdb621b
Doug Tyler <dougwt@gmail.com>
2013-12-03 16:02:17 -0800

    Clean up initial placeholder code

--- pyrate.py
+++ pyrate.py
@@ -10,43 +10,43 @@ class Inbound(threading.Thread):
 
     def run(self):
         while True:
-            item = self.in_queue.get()
+            # item = self.in_queue.get()
 
-            result = 'You should be doing work.'
-            self.out_queue.put(result)
+            # result = 'You should be doing work.'
+            # self.out_queue.put(result)
 
-            self.in_queue.task_done()
+            # self.in_queue.task_done()
+            pass
 
 class Outbound(threading.Thread):
-    def __init__(self, out_queue):
+    def __init__(self, in_queue, out_queue):
         threading.Thread.__init__(self)
+        self.in_queue = in_queue
         self.out_queue = out_queue
 
     def run(self):
         while True:
-            item = self.out_queue.get()
+            # item = self.out_queue.get()
 
-            result = 'This is your awesome output.'
+            # result = 'This is your awesome output.'
 
-            self.out_queue.task_done()
+            # self.out_queue.task_done()
+            pass
 
 if __name__ == '__main__':
 
     in_queue = Queue.Queue()
     out_queue = Queue.Queue()
 
-    for i in xrange(len(item_list)):
-       t = Inbound(in_queue, out_queue)
-       t.daemon = True
-       t.start()
+    # Launch Inbound thread
+    t = Inbound(in_queue, out_queue)
+    t.daemon = True
+    t.start()
 
-    for item in item_list:
-        in_queue.put(item)
+    # Launch Outbound thread
+    t = Outbound(out_queue)
+    t.daemon = True
+    t.start()
 
-    for i in xrange(len(item_list)):
-        t = Outbound(out_queue)
-        t.daemon = True
-        t.start()
-
-    in_queue.join()
-    out_queue.join()
+    # in_queue.join()
+    # out_queue.join()
